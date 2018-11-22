5e717e44f176ff88923fa3579389063ba1bb0ed6
StewartONeill <stewart.oneill@postgrad.manchester.ac.uk>
2018-11-22 11:19:29 +0000

    WRITE EXON NUMBER, EXON START & EXON STOP TO TSV

--- Exon_Parsing.py
+++ Exon_Parsing.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-
+import csv
 
 #Read in XML file.
 file = "../LRG_XML/LRG_1.xml"
@@ -7,10 +7,27 @@ tree = ET.parse(file)
 root = tree.getroot()
  
 #Finds all exon elements within the fixed_annotation/transcript element. 
-#Prints the exon tag and attribute
-for exon in root.findall("./fixed_annotation/transcript/exon"):
-#
-#/[@coord_system='LRG_1']"):
-	print(exon.tag, exon.attrib)
+#Prints the exon attribute
+#Prints the genomic start and end coords on this exon
 
-	 
+bed_array = [['exon_number', 'exon_start', 'exon_end']]
+
+lrg_id =root.find("./fixed_annotation/id").text
+print("LRG_ID is...", lrg_id)
+
+for exon in root.findall("./fixed_annotation/transcript/exon"):
+	exon_number = (exon.get("label"))
+	coords = exon.find("coordinates")
+	coords_start = (coords.get("start"))
+	coords_end = (coords.get("end"))
+	list = [exon_number, coords_start, coords_end]
+	bed_array.append(list)
+print(bed_array)
+
+filename = lrg_id + ".bed"
+print(filename)
+
+with open(filename, 'w') as tsvFile:
+	writer = csv.writer(tsvFile, delimiter='\t')
+	writer.writerows(bed_array)
+tsvFile.close()
