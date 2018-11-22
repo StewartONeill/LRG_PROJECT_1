323fefccbf49d8508008e1d7a40416cf01feebdd
StewartONeill <stewart.oneill@postgrad.manchester.ac.uk>
2018-11-22 12:01:35 +0000

    PUT INTO FUNCTION. ADD ABILITY TO INPUT FILE.

--- Exon_Parsing.py
+++ Exon_Parsing.py
@@ -1,33 +1,53 @@
 import xml.etree.ElementTree as ET
 import csv
+import argparse
 
-#Read in XML file.
-file = "../LRG_XML/LRG_1.xml"
-tree = ET.parse(file)
-root = tree.getroot()
- 
-#Finds all exon elements within the fixed_annotation/transcript element. 
-#Prints the exon attribute
-#Prints the genomic start and end coords on this exon
+def get_arguments():
 
-bed_array = [['exon_number', 'exon_start', 'exon_end']]
+	#Retrieve command line arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--file", help="path to xml file")
+	args = parser.parse_args()
+	filepath = args.file
+	print(filepath)
+	return filepath
+	
 
-lrg_id =root.find("./fixed_annotation/id").text
-print("LRG_ID is...", lrg_id)
+def lrg2bed(lrg_xml):
 
-for exon in root.findall("./fixed_annotation/transcript/exon"):
-	exon_number = (exon.get("label"))
-	coords = exon.find("coordinates")
-	coords_start = (coords.get("start"))
-	coords_end = (coords.get("end"))
-	list = [exon_number, coords_start, coords_end]
-	bed_array.append(list)
-print(bed_array)
+	'''takes lrg.xml file as argument and outputs bedfile with exon_number, start and stop coordinates'''
 
-filename = lrg_id + ".bed"
-print(filename)
+	#Read in XML file.
+	file = lrg_xml
+	tree = ET.parse(file)
+	root = tree.getroot()
+	 
+	#Finds all exon elements within the fixed_annotation/transcript element. 
+	#Prints the exon attribute
+	#Prints the genomic start and end coords on this exon
 
-with open(filename, 'w') as tsvFile:
-	writer = csv.writer(tsvFile, delimiter='\t')
-	writer.writerows(bed_array)
-tsvFile.close()
+	bed_array = [['exon_number', 'exon_start', 'exon_end']]
+
+	lrg_id =root.find("./fixed_annotation/id").text
+	print("LRG_ID is...", lrg_id)
+
+	for exon in root.findall("./fixed_annotation/transcript/exon"):
+		exon_number = (exon.get("label"))
+		coords = exon.find("coordinates")
+		coords_start = (coords.get("start"))
+		coords_end = (coords.get("end"))
+		list = [exon_number, coords_start, coords_end]
+		bed_array.append(list)
+	print(bed_array)
+
+	filename = lrg_id + ".bed"
+	print(filename)
+
+	with open(filename, 'w') as tsvFile:
+		writer = csv.writer(tsvFile, delimiter='\t')
+		writer.writerows(bed_array)
+	tsvFile.close()
+
+if __name__ == '__main__':
+	filepath = get_arguments()
+	lrg2bed(filepath)
