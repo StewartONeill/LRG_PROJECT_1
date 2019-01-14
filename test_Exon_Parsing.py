c81608cfdd1de138505fb824bfd3b9e694aab1ea
ashley-pritchard <ashley.pritchard@ouh.nhs.uk>
2019-01-14 15:58:56 +0000

    add comment to code

--- test_Exon_Parsing.py
+++ test_Exon_Parsing.py
@@ -4,5 +4,6 @@ import pandas as pd
 import argparse
 import pytest
 
+#tests if the correct gene name is derived when 'lrg_no=5' is used as input
 def test_one():
 	assert lrg2bed(outpath="./", lrg_no="5", filepath=None)[1]=="C1orf50"
